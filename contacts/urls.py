1647856c1ce64fb168056fa55a29df0484db0566
Nishant <nishaantkumar09@gmail.com>
2022-06-28 23:24:58 +0530

    enter

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add-contact/', views.addContact, name='add-contact'),
+    
+    path('profile/<str:pk>', views.contactProfile, name='profile'),
     path('edit-contact/<str:pk>', views.editContact, name='edit-contact'),
+ 
 ]
    
--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-
 from .models import Contact
 
 # Create your views here.
+
 def index(request):
     contacts = Contact.objects.all()
     search_input = request.GET.get('search-area')
@@ -12,9 +12,6 @@ def index(request):
         contacts = Contact.objects.all()
         search_input = ''
     return render(request, 'index.html', {'contacts': contacts, 'search_input': search_input})
-
-    
-    return render(request,'index.html')
 
 def addContact(request):
     if request.method == 'POST':
@@ -28,11 +25,35 @@ def addContact(request):
             )
         new_contact.save()
         return redirect('/')
+        def editContact(request, pk):
+         contact = Contact.objects.get(id=pk)
 
+def editContact(request, pk):
+    contact = Contact.objects.get(id=pk)
 
-    return render(request, 'new.html')
-    
-    
+    if request.method == 'POST':
+        Contact.full_name = request.POST['fullname']
+        Contact.relationship = request.POST['relationship']
+        Contact.email = request.POST['email']
+        Contact.phone_number = request.POST['phone-number']
+        Contact.address = request.POST['address']
+        Contact.save()
+        
+        return redirect('/profile/'+str(Contact.id))
+    return render(request, 'edit.html', {'contacts': Contact})
+
+def deleteContact(request, pk):
+    contact = Contact.objects.get(id=pk)
+
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('/')
+
+    return render(request, 'delete.html', {'contacts': contact})
+
+def contactProfile(request, pk):
+    contact = Contact.objects.get(id=pk)
+    return render(request, 'contact-profile.html', {'contacts':contact})
 
         
 
